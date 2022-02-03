45c8fce11ab26a746a74949ccbcd7416ba341508
Phillipp Schoppmann <schoppmann@google.com>
2022-02-03 11:01:10 -0800

    Add benchmark for DCF evaluation
    PiperOrigin-RevId: 426189230

---
+++ dcf/distributed_comparison_function_benchmark.cc
@@ -0,0 +1,56 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "absl/numeric/int128.h"
+#include "absl/random/random.h"
+#include "benchmark/benchmark.h"
+#include "dcf/distributed_comparison_function.h"
+#include "dcf/distributed_comparison_function.pb.h"
+#include "dpf/distributed_point_function.h"
+
+namespace distributed_point_functions {
+
+template <typename T>
+void BM_EvaluateDcf(benchmark::State& state) {
+  int log_domain_size = state.range(0);
+  DcfParameters parameters;
+  *(parameters.mutable_parameters()->mutable_value_type()) = ToValueType<T>();
+  parameters.mutable_parameters()->set_log_domain_size(log_domain_size);
+  std::unique_ptr<DistributedComparisonFunction> dcf =
+      DistributedComparisonFunction::Create(parameters).value();
+
+  absl::BitGen rng;
+  absl::uint128 domain_mask = absl::Uint128Max();
+  absl::uint128 alpha = absl::Uniform<absl::uint128>(rng);
+  if (log_domain_size < 128) {
+    domain_mask = (absl::uint128{1} << log_domain_size) - 1;
+  }
+  alpha &= domain_mask;
+  T beta(42);
+  DcfKey key;
+  std::tie(key, std::ignore) = dcf->GenerateKeys(alpha, beta).value();
+  absl::uint128 x = 0;
+  for (auto s : state) {
+    T evaluation = dcf->Evaluate<T>(key, x).value();
+    x = (x + 1) & domain_mask;
+    benchmark::DoNotOptimize(evaluation);
+  }
+}
+BENCHMARK_TEMPLATE(BM_EvaluateDcf, uint8_t)->DenseRange(2, 24, 2);
+BENCHMARK_TEMPLATE(BM_EvaluateDcf, uint16_t)->DenseRange(2, 24, 2);
+BENCHMARK_TEMPLATE(BM_EvaluateDcf, uint32_t)->DenseRange(2, 24, 2);
+BENCHMARK_TEMPLATE(BM_EvaluateDcf, uint64_t)->DenseRange(2, 24, 2);
+BENCHMARK_TEMPLATE(BM_EvaluateDcf, absl::uint128)->DenseRange(2, 24, 2);
+
+}  // namespace distributed_point_functions
