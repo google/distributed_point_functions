cfde1154463c4b191407016eefd75de7565a1373
Phillipp Schoppmann <schoppmann@google.com>
2023-11-08 11:29:41 -0800

    Simplify testing of CuckooHashedDpfPirDatabase and consistently use mocks for wrapped classes
    PiperOrigin-RevId: 580597679

--- pir/cuckoo_hashed_dpf_pir_database_test.cc
+++ pir/cuckoo_hashed_dpf_pir_database_test.cc
@@ -28,6 +28,7 @@
 #include "absl/status/statusor.h"
 #include "absl/types/span.h"
 #include "dpf/internal/status_matchers.h"
+#include "dpf/xor_wrapper.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "pir/dense_dpf_pir_database.h"
@@ -55,11 +56,15 @@ using ::testing::HasSubstr;
 using ::testing::NotNull;
 using ::testing::Pointee;
 using ::testing::Property;
+using ::testing::Return;
+using ::testing::ReturnRef;
 using ::testing::StartsWith;
 using ::testing::Truly;
 using Database = CuckooHashedDpfPirDatabase::Interface;
-using MockDenseBuilder = pir_testing::MockPirDatabase<XorWrapper<absl::uint128>,
-                                                      std::string>::Builder;
+using DenseDatabase = CuckooHashedDpfPirDatabase::DenseDatabase;
+using MockDenseDatabase =
+    pir_testing::MockPirDatabase<XorWrapper<absl::uint128>, std::string>;
+using MockDenseBuilder = MockDenseDatabase::Builder;
 
 TEST(CuckooHashedDpfPirDatabaseBuilder, SetParamsFailsIfNumBucketsIsZero) {
   CuckooHashedDpfPirDatabase::Builder builder;
@@ -96,41 +101,25 @@ class CuckooHashedDpfPirDatabaseBuilderTest : public ::testing::Test {
     params.mutable_hash_family_config()->set_seed("A seed");
     builder_.SetParams(params);
 
-    // Set up dense builders for keys and values.
+    // Set up dense mock builders for keys and values.
     mock_key_builder_ = std::make_unique<MockDenseBuilder>();
     mock_value_builder_ = std::make_unique<MockDenseBuilder>();
-    // Save plain pointers to pass them into the lambdas below, because the
-    // unique_ptrs might have been moved from when they are called.
-    MockDenseBuilder* mock_key_builder_ptr = mock_key_builder_.get();
-    MockDenseBuilder* mock_value_builder_ptr = mock_value_builder_.get();
     ON_CALL(*mock_key_builder_, Insert)
-        .WillByDefault(
-            [this, mock_key_builder_ptr](std::string input) -> auto& {
-              real_key_builder_.Insert(input);
-              return *mock_key_builder_ptr;
-            });
+        .WillByDefault(ReturnRef(*mock_key_builder_));
     ON_CALL(*mock_key_builder_, Clear)
-        .WillByDefault([this, mock_key_builder_ptr]() -> auto& {
-          real_key_builder_.Clear();
-          return *mock_key_builder_ptr;
-        });
-    ON_CALL(*mock_key_builder_, Build).WillByDefault([this]() {
-      return real_key_builder_.Build();
-    });
+        .WillByDefault(ReturnRef(*mock_key_builder_));
     ON_CALL(*mock_value_builder_, Insert)
-        .WillByDefault(
-            [this, mock_value_builder_ptr](std::string input) -> auto& {
-              real_value_builder_.Insert(input);
-              return *mock_value_builder_ptr;
-            });
+        .WillByDefault(ReturnRef(*mock_value_builder_));
     ON_CALL(*mock_value_builder_, Clear)
-        .WillByDefault([this, mock_value_builder_ptr]() -> auto& {
-          real_value_builder_.Clear();
-          return *mock_value_builder_ptr;
-        });
-    ON_CALL(*mock_value_builder_, Build).WillByDefault([this]() {
-      return real_value_builder_.Build();
-    });
+        .WillByDefault(ReturnRef(*mock_value_builder_));
+
+    // Set up mock databases.
+    mock_key_database_ = std::make_unique<MockDenseDatabase>();
+    mock_value_database_ = std::make_unique<MockDenseDatabase>();
+    ON_CALL(*mock_key_database_, num_selection_bits)
+        .WillByDefault(Return(kNumBuckets));
+    ON_CALL(*mock_value_database_, num_selection_bits)
+        .WillByDefault(Return(kNumBuckets));
   }
 
   void InsertElements() {
@@ -147,7 +136,7 @@ class CuckooHashedDpfPirDatabaseBuilderTest : public ::testing::Test {
   std::vector<std::string> keys_, values_;
   CuckooHashedDpfPirDatabase::Builder builder_;
   std::unique_ptr<MockDenseBuilder> mock_key_builder_, mock_value_builder_;
-  DenseDpfPirDatabase::Builder real_key_builder_, real_value_builder_;
+  std::unique_ptr<MockDenseDatabase> mock_key_database_, mock_value_database_;
 };
 
 TEST_F(CuckooHashedDpfPirDatabaseBuilderTest,
@@ -175,7 +164,7 @@ TEST_F(CuckooHashedDpfPirDatabaseBuilderTest,
 TEST_F(CuckooHashedDpfPirDatabaseBuilderTest, BuildsEmptyDatabase) {
   EXPECT_THAT(
       builder_.Build(),
-      IsOkAndHolds(Truly([](auto& db) -> bool { return db->size() == 0; })));
+      IsOkAndHolds(Truly([](auto &db) -> bool { return db->size() == 0; })));
 }
 
 TEST_F(CuckooHashedDpfPirDatabaseBuilderTest, FailsToBuildWithEmptyKey) {
@@ -203,32 +192,37 @@ TEST_F(CuckooHashedDpfPirDatabaseBuilderTest, UsesKeyBuilderCorrectly) {
   const std::string key = "Key 1";
   const std::string value = "Value 1";
 
-  EXPECT_CALL(*mock_key_builder_, Insert).Times(AtLeast(1));
-  EXPECT_CALL(*mock_key_builder_, Build).Times(1);
+  EXPECT_CALL(*mock_key_builder_, Insert(key)).Times(1);
+  EXPECT_CALL(*mock_key_builder_, Insert("")).Times(kNumBuckets - 1);
+  EXPECT_CALL(*mock_key_builder_, Build)
+      .WillOnce(Return(std::move(mock_key_database_)));
+
   builder_.Insert({key, value})
       .SetKeyDatabaseBuilder(std::move(mock_key_builder_));
 
-  EXPECT_THAT(builder_.Build(),
-              IsOkAndHolds(Pointee(Property(&Database::size, Eq(1)))));
+  EXPECT_THAT(builder_.Build(), IsOkAndHolds(NotNull()));
 }
 
 TEST_F(CuckooHashedDpfPirDatabaseBuilderTest, UsesValueBuilderCorrectly) {
   const std::string key = "Key 1";
   const std::string value = "Value 1";
 
-  EXPECT_CALL(*mock_value_builder_, Insert).Times(AtLeast(1));
-  EXPECT_CALL(*mock_value_builder_, Build).Times(1);
+  EXPECT_CALL(*mock_value_builder_, Insert(value)).Times(1);
+  EXPECT_CALL(*mock_value_builder_, Insert("")).Times(kNumBuckets - 1);
+  EXPECT_CALL(*mock_value_builder_, Build)
+      .WillOnce(Return(std::move(mock_value_database_)));
+
   builder_.Insert({key, value})
       .SetValueDatabaseBuilder(std::move(mock_value_builder_));
 
-  EXPECT_THAT(builder_.Build(),
-              IsOkAndHolds(Pointee(Property(&Database::size, Eq(1)))));
+  EXPECT_THAT(builder_.Build(), IsOkAndHolds(NotNull()));
 }
 
 TEST_F(CuckooHashedDpfPirDatabaseBuilderTest,
        CloneCallsDenseDatabaseCloneCorrectly) {
   EXPECT_CALL(*mock_key_builder_, Clone).Times(1);
   EXPECT_CALL(*mock_value_builder_, Clone).Times(1);
+
   builder_.SetKeyDatabaseBuilder(std::move(mock_key_builder_))
       .SetValueDatabaseBuilder(std::move(mock_value_builder_));
 
@@ -274,6 +268,7 @@ TEST_F(CuckooHashedDpfPirDatabaseBuilderTest, ComputesInnerProductCorrectly) {
       pir_testing::PackSelectionBits<Database::BlockType>(
           std::vector<bool>(database->num_selection_bits(), true));
 
+  // We expect the result to be the XOR of all keys, padded with zeros.
   std::string key, value;
   for (int i = 0; i < kNumDatabaseElements; ++i) {
     key.resize(std::max(key.size(), keys_[i].size()), '\0');
@@ -285,9 +280,37 @@ TEST_F(CuckooHashedDpfPirDatabaseBuilderTest, ComputesInnerProductCorrectly) {
       value[j] ^= values_[i][j];
     }
   }
+
   EXPECT_THAT(
       database->InnerProductWith({selections}),
       IsOkAndHolds(ElementsAre(FieldsAre(StartsWith(key), StartsWith(value)))));
+}
+
+TEST_F(CuckooHashedDpfPirDatabaseBuilderTest, CallsInnerProductWithCorrectly) {
+  InsertElements();
+
+  std::vector<Database::BlockType> selections =
+      pir_testing::PackSelectionBits<Database::BlockType>(
+          std::vector<bool>(kNumBuckets, true));
+
+  EXPECT_CALL(*mock_key_database_, InnerProductWith(ElementsAre(selections)))
+      .WillOnce(Return(std::vector<std::string>{"dummy key"}));
+  EXPECT_CALL(*mock_value_database_, InnerProductWith(ElementsAre(selections)))
+      .WillOnce(Return(std::vector<std::string>{"dummy value"}));
+  EXPECT_CALL(*mock_key_builder_, Build)
+      .WillOnce(Return(std::move(mock_key_database_)));
+  EXPECT_CALL(*mock_value_builder_, Build)
+      .WillOnce(Return(std::move(mock_value_database_)));
+
+  DPF_ASSERT_OK_AND_ASSIGN(
+      std::unique_ptr<Database> database,
+      builder_.SetKeyDatabaseBuilder(std::move(mock_key_builder_))
+          .SetValueDatabaseBuilder(std::move(mock_value_builder_))
+          .Build())
+
+  EXPECT_THAT(database->InnerProductWith({selections}),
+              IsOkAndHolds(ElementsAre(FieldsAre(StartsWith("dummy key"),
+                                                 StartsWith("dummy value")))));
 }
 
 TEST_F(CuckooHashedDpfPirDatabaseBuilderTest,
